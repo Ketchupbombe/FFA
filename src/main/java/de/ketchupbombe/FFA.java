823e2ecdea6665b28fed993af8a8c618ed4fa359
Ketchupbombe <LukasHonold1407@gmail.com>
2017-08-01 15:07:34 +0200

    create Project

---
+++ src/main/java/de/ketchupbombe/FFA.java
@@ -0,0 +1,16 @@
+package de.ketchupbombe;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FFA extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+
+}
