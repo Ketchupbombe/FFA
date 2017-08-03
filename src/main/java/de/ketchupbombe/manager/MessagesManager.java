1530f83de3cd621a9b2d3ba6ab447eed32bdfb73
Ketchupbombe <LukasHonold1407@gmail.com>
2017-08-03 14:41:18 +0200

    Messages Manager / commits

--- src/main/java/de/ketchupbombe/manager/MessagesManager.java
+++ src/main/java/de/ketchupbombe/manager/MessagesManager.java
@@ -16,6 +16,9 @@ public class MessagesManager {
     private File file = null;
     private FileConfiguration cfg = null;
 
+    /**
+     * Create new File called "Messages.yml" in datafolder
+     */
     public void createMessagesConfig() {
         this.file = new File(FFA.getInstance().getDataFolder(), "Messages.yml");
         this.cfg = YamlConfiguration.loadConfiguration(file);
@@ -23,22 +26,43 @@ public class MessagesManager {
         saveFile();
     }
 
-    public void saveFile() {
+    /**
+     * Save file
+     */
+    private void saveFile() {
         try {
             cfg.save(file);
         } catch (IOException e) {
         }
     }
 
+    /**
+     * Add a message to Messages.yml
+     *
+     * @param path  which path
+     * @param value Message
+     */
     public void addMessage(String path, String value) {
         cfg.addDefault(path, value);
         saveFile();
     }
 
+    /**
+     * Get a Message from Messages.yml
+     *
+     * @param path place to find value
+     * @return value of path
+     */
     public String getMessage(String path) {
         return cfg.getString(path);
     }
 
+    /**
+     * Find out if path exist
+     *
+     * @param path place to find value
+     * @return if path exists
+     */
     public boolean isPathExist(String path) {
         return cfg.get(path) != null;
     }
